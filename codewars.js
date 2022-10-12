b0610e20e33e249f3f1a4443a29d384ee5af4361
Sabrina Keeler <sabrina.s.keeler@gmail.com>
2022-10-12 14:20:01 -0400

    century kata 8

--- codewars.js
+++ codewars.js
@@ -12,3 +12,12 @@ function getMiddle(s) {
 }
 
 getMiddle("test")
+
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+function century(year) {
+
+    console.log(Math.ceil(year / 100));
+}
+
+century(1901)
