4eb4b3ecce18f9bc9b777261efa2dc5ba99dc6f0
Sabrina Keeler <sabrina.s.keeler@gmail.com>
2022-10-12 14:26:45 -0400

    multiply java kata 8

---
+++ codewars.java
@@ -0,0 +1,12 @@
+// This code does not execute properly. Try to figure out why.
+// function multiply(a, b){
+//   a * b
+// }
+
+
+
+public class Multiply {
+    public static Double multiply(Double a, Double b) {
+        return a * b;
+    }
+}
