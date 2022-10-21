d70f2c4287a4aebb0b7cc1db02c72d4bf02798c2
Sabrina Keeler <sabrina.s.keeler@gmail.com>
2022-10-21 18:11:17 -0400

    liftoff coding challenge

--- codewars.js
+++ codewars.js
@@ -142,3 +142,12 @@ console.log(highAndLow("1 2 3 4 5"), "5 1") // return "5 1"
 highAndLow("1 2 -3 4 5"), "5 -3" // return "5 -3"
 highAndLow("1 9 3 4 -5"), "9 -5"// return "9 -5"
 console.log(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"), "42 -9") 
+
+
+// Edabit Coding challenge
+// find area of a triangle
+function triArea(base, height) {
+    let area = (base * height) / 2;
+    return area;
+
+}
