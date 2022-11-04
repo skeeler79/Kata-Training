f691a6b0db2ebae444c686d0739be441477471e9
Sabrina Keeler <sabrina.s.keeler@gmail.com>
2022-11-04 14:46:31 -0400

    decendingOrder kata 7

--- codewars.js
+++ codewars.js
@@ -252,3 +252,23 @@ console.log(FirstReverse("Hello World!"))
 //R - returns the reverse of the string
 //E- Hello --> olleH
 //
+
+
+
+// Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+// Examples:
+// Input: 42145 Output: 54421
+
+// Input: 145263 Output: 654321
+
+// Input: 123456789 Output: 987654321
+
+function descendingOrder(n) {
+    let stringN = String(n);
+    let decOrd = stringN.split('').sort(function (a, b) { return b - a }).join('');
+    return Number(decOrd)
+}
+
+
+console.log(descendingOrder(42145), 54421)
