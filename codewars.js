2b16b13a2af01b3b332cf73df7b7ef21c6743eb4
Sabrina Keeler <sabrina.s.keeler@gmail.com>
2022-12-05 11:58:24 -0500

    finds lowercase letters in a string

--- codewars.js
+++ codewars.js
@@ -302,3 +302,34 @@ function LongestWord(sen) {
 
 // keep this function call here 
 console.log(LongestWord("The quick red*&^%((())) fox"));
+
+
+
+
+// A word is on the loose and now has tried to hide amongst a crowd of tall letters! Help write a function to detect what the word is, knowing the following rules:
+
+// The wanted word is in lowercase.
+// The crowd of letters is all in uppercase.
+// Note that the word will be spread out amongst the random letters, but their letters remain in the same order.
+
+// detectWord("UcUNFYGaFYFYGtNUH") ➞ "cat"
+
+// detectWord("bEEFGBuFBRrHgUHlNFYaYr") ➞ "burglar"
+
+// detectWord("YFemHUFBbezFBYzFBYLleGBYEFGBMENTment") ➞ "embezzlement"
+
+function detectWord(str) {
+    let wordArr = '';
+    for (let i = 0; i < str.length; i++) {
+        // console.log(str[i])
+        if (str[i].toLowerCase() == str[i]) {
+
+            wordArr += str[i]
+        }
+
+    }
+    return wordArr;
+}
+console.log(detectWord("UcUNFYGaFYFYGtNUH"), 'cat');
+console.log(detectWord("bEEFGBuFBRrHgUHlNFYaYr"), 'burglur');
+console.log(detectWord("YFemHUFBbezFBYzFBYLleGBYEFGBMENTment"), 'embezzlement')
