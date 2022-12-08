53b0e5d8b46ee3adb1322c0f47d8a70207e224ef
Sabrina Keeler <sabrina.s.keeler@gmail.com>
2022-12-08 15:51:37 -0500

    filter function

--- codewars.js
+++ codewars.js
@@ -333,3 +333,27 @@ function detectWord(str) {
 console.log(detectWord("UcUNFYGaFYFYGtNUH"), 'cat');
 console.log(detectWord("bEEFGBuFBRrHgUHlNFYaYr"), 'burglur');
 console.log(detectWord("YFemHUFBbezFBYzFBYLleGBYEFGBMENTment"), 'embezzlement')
+
+
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+function filter_list(l) {
+  let numArr = [] //new array has to be within the function so that it clears with each new function call
+  // Return a new array with the strings filtered out
+  for (let i = 0; i < l.length; i++){
+    if (Number.isInteger(l[i]))
+    numArr.push(l[i]);
+
+    }
+   return numArr
+  }
+
+
+console.log(filter_list([1,2,'aasf','1','123',123]))
+
+//refactored using .filter function
+
+function filter_list(l) {
+    return l.filter(Number.isInteger)
+}
+console.log(filter_list([1, 2, 'aasf', '1', '123', 123]))
+
