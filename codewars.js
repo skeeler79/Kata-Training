81008125e79c454b950c11723439ad7346a0fbb0
Sabrina Keeler <sabrina.s.keeler@gmail.com>
2023-02-26 17:02:24 -0500

    DNAStrand exercise using switch case

--- codewars.js
+++ codewars.js
@@ -421,3 +421,32 @@ function isIsogram(input) {
     }
 }
 
+
+// Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+// More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+// Example: (input --> output)
+
+// "ATTGC" --> "TAACG"
+// "GTAT" --> "CATA"
+
+function DNAStrand(dna) {
+  let complement = dna.replace(/[ATTGC]/g, function (match) {
+    switch (match) {
+      case "A":
+        return "T";
+      case "T":
+        return "A";
+      case "C":
+        return "G";
+      case "G":
+        return "C";
+    }
+  });
+
+  console.log(complement);
+}
+
